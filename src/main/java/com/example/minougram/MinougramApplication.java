defac318b3e50a0649111e04fa7aac23e4c54c1b
chiharu <sharpay_chiharu@yahoo.co.jp>
2024-10-22 22:24:45 +0900

    first commit

---
+++ src/main/java/com/example/minougram/MinougramApplication.java
@@ -0,0 +1,13 @@
+package com.example.minougram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinougramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinougramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minougram/MinougramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minougram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinougramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
